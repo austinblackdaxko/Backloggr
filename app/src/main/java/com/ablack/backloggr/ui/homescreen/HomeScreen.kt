a8ef91c1ca080f278772168297aeb2ff8da28f73
Austin Black <austin.black@daxko.com>
2023-03-08 13:49:06 -0600

    created game object and displayed on homescreen

--- app/src/main/java/com/ablack/backloggr/ui/homescreen/HomeScreen.kt
+++ app/src/main/java/com/ablack/backloggr/ui/homescreen/HomeScreen.kt
@@ -1,10 +1,50 @@
 package com.ablack.backloggr.ui.homescreen
 
+import android.graphics.Color
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.ablack.backloggr.R
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
-    Text(text = viewModel.title)
+//    Text(text = viewModel.title)
+    val scrollState = rememberScrollState()
+    Row(modifier = Modifier.verticalScroll(scrollState)) {
+
+        val image: Painter = painterResource(id = R.drawable.hollowknight)
+        Image(
+            painter = image,
+            contentDescription = "gamecover",
+            contentScale = ContentScale.Crop,            // crop the image if it's not a square
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)                       // clip to the circle shape
+        )
+
+        Text(
+            text = viewModel.videoGame.title,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
--- app/src/main/java/com/ablack/backloggr/ui/homescreen/HomeViewModel.kt
+++ app/src/main/java/com/ablack/backloggr/ui/homescreen/HomeViewModel.kt
@@ -1,10 +1,20 @@
 package com.ablack.backloggr.ui.homescreen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.activity.compose.setContent
 import androidx.lifecycle.ViewModel
+import com.ablack.backloggr.R
+import com.ablack.backloggr.data.VideoGame
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor() : ViewModel() {
     val title : String = "hello world"
+    val videoGame = VideoGame("Hollow Knight", "Metroidvania", "A great video" +
+            "game", R.drawable.hollowknight )
+    val vgtitle : String = videoGame.title
+
 }
