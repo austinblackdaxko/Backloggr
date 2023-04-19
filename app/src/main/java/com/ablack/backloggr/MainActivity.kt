52cf46febc0b8ce5c10280a7af232586542a76cb
Austin Black <austin.black@daxko.com>
2023-04-19 13:48:55 -0500

    removed video game variables

--- app/src/main/java/com/ablack/backloggr/MainActivity.kt
+++ app/src/main/java/com/ablack/backloggr/MainActivity.kt
@@ -3,13 +3,13 @@ package com.ablack.backloggr
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.ablack.backloggr.ui.homescreen.HomeScreen
 import com.ablack.backloggr.ui.theme.BackloggrTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,15 +19,29 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             BackloggrTheme {
-                HomeScreen()
+                MyNavHost(navController = navController)
             }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
+fun MyNavHost(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "Home") {
+        composable("Home") {
+            HomeScreen()
+        }
+        composable("Search") {
+            SearchScreen("Austin")
+        }
+    }
+}
+
+@Composable
+fun SearchScreen(name: String) {
     Text(text = "Hello $name!")
 }
 
@@ -35,6 +49,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     BackloggrTheme {
-        Greeting("Android")
+        SearchScreen("Android")
     }
 }
--- app/src/main/java/com/ablack/backloggr/ui/homescreen/HomeScreen.kt
+++ app/src/main/java/com/ablack/backloggr/ui/homescreen/HomeScreen.kt
@@ -1,21 +1,18 @@
 package com.ablack.backloggr.ui.homescreen
 
-import android.graphics.Color
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -24,7 +21,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ablack.backloggr.R
-import com.ablack.backloggr.data.VideoGame
 import com.ablack.backloggr.data.network.models.TVShow
 
 
@@ -35,16 +31,19 @@ fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
         viewModel.fetchTVShows()
     }
 
-//    Text(text = viewModel.title)
-    val scrollState = rememberScrollState()
-//    GameItem(videoGame = viewModel.videoGame)
-
-    LazyColumn() {
-        items(viewModel.tvShows) {
-            item ->  GameItem(tvShow = item)
+    Scaffold(bottomBar = {
+        Row(modifier = Modifier.fillMaxWidth().background(Color.Magenta).padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "Home", fontSize = 20.sp)
+            Text(text = "Search", fontSize = 20.sp)
+        }
+    }) {
+        LazyColumn(modifier = Modifier.padding(bottom = it.calculateBottomPadding())) {
+            items(viewModel.tvShows) {
+                    item ->  GameItem(tvShow = item)
+            }
         }
     }
-
 }
 
 @Composable
